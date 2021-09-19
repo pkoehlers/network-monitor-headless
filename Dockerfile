4fc3e2c3b7e1f12cfd7ce123264c83f8ac392bc4
pkoehlers <50806567+pkoehlers@users.noreply.github.com>
2021-09-19 20:26:27 +0200

    Clean old project names, non root docker user

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,18 @@
 FROM node:current-buster
 
+RUN groupadd --gid 5000 networkmonitor \
+    && useradd --home-dir /home/networkmonitor --create-home --uid 5000 \
+        --gid 5000 --shell /bin/sh --skel /dev/null networkmonitor
+
 WORKDIR /usr/src/app
 
 COPY package*.json ./
-
 RUN npm ci --only=production
+
 COPY dist .
+
+RUN chown -R networkmonitor:networkmonitor .
+
+USER networkmonitor
 
 CMD [ "node", "server.js" ]
--- rpi/setup.sh
+++ rpi/setup.sh
@@ -6,14 +6,18 @@ cd /tmp/setup
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
-sudo useradd -g docker speedtest
-sudo mkdir -p /opt/speedtest/log
-sudo mkdir -p /opt/speedtest/config
-sudo chown -R speedtest:docker /opt/speedtest
-sudo chmod -R 740 /opt/speedtest
-sudo curl -o /opt/speedtest/config/config.json https://raw.githubusercontent.com/pkoehlers/network-monitor-headless/master/config.json.example
-sudo vi /opt/speedtest/config/config.json
+groupadd --gid 5000 networkmonitor
+useradd --home-dir /home/networkmonitor --create-home --uid 5000 \
+        --gid 5000 --shell /bin/sh --skel /dev/null networkmonitor
 
-sudo curl -o /etc/systemd/system/speedtest.service https://raw.githubusercontent.com/pkoehlers/network-monitor-headless/master/systemd/speedtest.service
+sudo mkdir -p /home/networkmonitor/log
+sudo mkdir -p /home/networkmonitor/config
+sudo curl -o /home/networkmonitor/config/config.json https://raw.githubusercontent.com/pkoehlers/network-monitor-headless/master/config.json.example
+sudo vi /home/networkmonitor/config/config.json
+sudo chown -R networkmonitor:docker /home/networkmonitor
+sudo chmod -R 740 /home/networkmonitor
+
+
+sudo curl -o /etc/systemd/system/networkmonitor.service https://raw.githubusercontent.com/pkoehlers/network-monitor-headless/master/systemd/networkmonitor.service
 sudo systemctl daemon-reload
-sudo systemctl enable speedtest
+sudo systemctl enable networkmonitor
